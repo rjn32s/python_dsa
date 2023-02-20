8250a451c9f81952e2714050e22c5ffe6b589aa7
Rajan Shukla <94681756+rjn32s@users.noreply.github.com>
2023-02-20 14:05:35 +0000

    Added the code but not completed

---
+++ heap_sort.py
@@ -0,0 +1,22 @@
+def heapify(arr,n,i):
+    largest = i
+    l = 2*i+1
+    r = 2*i+2
+
+    if l<n and arr[largest] < arr[r]:
+        largest = l
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+    
+    if largest != i:
+        arr[i] , arr[largest] = arr[largest],arr[i]
+        heapify(arr,n,largest)
+
+def heapSirt(arr):
+    n = len(arr)
+
+    for i in range(n//2 , -1 , -1):
+        heapify(arr,n,i)
+    for i in range(n-1 , 0 , -1):
+        heapify(arr,n,i)
+    
